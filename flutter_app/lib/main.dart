be851fba3e9dd877145a753609a693847d2b3f1f
soma <whitetrue2554@gmail.com>
2021-06-06 12:24:26 +0900

    add: normalPage

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/view/common/style.dart';
+import 'package:flutter_app/view/pages/normal.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,26 +14,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: AppThemeColor.primaryColor,
       ),
-      initialRoute: '/',
+      initialRoute: '/normal',
       onGenerateRoute: (RouteSettings settings) {
         switch (settings.name) {
-          case "/":
-            return MaterialPageRoute(builder: (context) => TopPage());
-          case "/setting":
-            return MaterialPageRoute(builder: (context) => TopPage());
-          case "/usage":
-            return MaterialPageRoute(builder: (context) => TopPage());
-          case "/help":
-            return MaterialPageRoute(builder: (context) => TopPage());
-          case "/restaurantlist":
-            return MaterialPageRoute(builder: (context) => TopPage());
-          case "/login":
-            return MaterialPageRoute(builder: (context) => TopPage());
-          case "/signin":
-            return MaterialPageRoute(builder: (context) => SigninPage());
-          case "/qrscan":
-            return MaterialPageRoute(
-                builder: (context) => QRViewExample(), fullscreenDialog: true);
+          // case "/":
+          //   return MaterialPageRoute(builder: (context) => TopPage());
+          case "/normal":
+            return MaterialPageRoute(builder: (context) => NormalPage());
           default:
             return null;
         }
